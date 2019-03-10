d079f203985b638622b6b2c851dd8a43f397ed01
Dylan Burk <burkdt03@uwgb.edu>
2019-03-10 00:31:05 -0600

    MoveMap Set Up
    I set up basic movement. Currently, everything but the Movement script is in the folder "MoveMap." The MoveMapTest scene is a quick test for movement and a possible way we could set up a map. Explaining the map would be easier in person.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 3f;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float mHor = Input.GetAxis("Horizontal");
+        float mVer = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(mHor, 0, mVer);
+
+        rb.velocity = move * speed;
+    }
+}
