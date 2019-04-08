da78313af9cffbe5155d5549d70122314f81cfee
Dylan Burk <burkdt03@uwgb.edu>
2019-04-08 09:15:53 -0500

    Health Change
    Changed "<" to "<=" in player's health check.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -55,7 +55,7 @@ public class PlayerController : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
-        if (health < 0)
+        if (health <= 0)
         {
             health = 0;
             gc.GameOver();
