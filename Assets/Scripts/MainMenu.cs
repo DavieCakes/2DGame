ad4ed89c7b42ad8e23a484897e83a177794e7282
Dylan Burk <burkdt03@uwgb.edu>
2019-04-12 10:02:37 -0500

    Main Menu
    Took the already made main menu and connected.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu, audioMenu, helpMenu;
+
+    private void Start()
+    {
+        audioMenu.SetActive(false);
+        helpMenu.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Alpha");
+    }
+
+    public void Audio()
+    {
+        mainMenu.SetActive(false);
+        audioMenu.SetActive(true);
+    }
+
+    public void Help()
+    {
+        mainMenu.SetActive(false);
+        helpMenu.SetActive(true);
+    }
+
+    public void Back()
+    {
+        audioMenu.SetActive(false);
+        helpMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
